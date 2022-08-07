c2137df6ab35386501908aab7c362a0ffa6f68f6
iamsirmike <asamoahmichael77@gmail.com>
2022-08-07 20:01:12 +0000

    modify responses

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,13 +6,13 @@ const verifyToken = (req, res, next) => {
   const token = req.headers["token"];
 
   if (!token) {
-    return res.status(403).send("You are not logged in. Log in to access this data");
+    return res.status(400).send("No user token provided");
   }
   try {
     const decoded = verify(token, process.env.JWT_TOKEN);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).send("You are not authorized to see this page");
   }
   return next();
 };
