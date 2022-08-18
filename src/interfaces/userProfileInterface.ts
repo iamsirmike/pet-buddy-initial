7e70b4178b3ed11b1ba3e56bbb75758f62f7399a
iamsirmike <asamoahmichael77@gmail.com>
2022-08-18 17:58:29 +0000

    create profile interface

--- src/interfaces/userProfileInterface.ts
+++ src/interfaces/userProfileInterface.ts
@@ -2,11 +2,8 @@ import { UserData } from "./userInterface";
 
 export interface UserProfileData extends UserData {
     userId: string;
-    username: string;
     email: string;
     firstname: string;
     lastname: string;
     phone: string;
-    password: string;
-    isVerified: boolean;
 }
